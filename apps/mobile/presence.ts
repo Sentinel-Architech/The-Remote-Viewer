d231e1bd097389bf0f9d33f4917f182001a7701b
The_Archetecht <charleshunt05@gmail.com>
2026-07-30 07:53:12 -0400

    Update presence.ts
    Signed-off-by: The_Archetecht <charleshunt05@gmail.com>

--- apps/mobile/presence.ts
+++ apps/mobile/presence.ts
@@ -21,6 +21,7 @@ export type DidKeyIdentity = {
 export async function createDidKey(): Promise<DidKeyIdentity> {
   // 100% local secure random
   const privateKey = new Uint8Array(32);
+crypto.getRandomValues(privateKey);
   crypto.getRandomValues(privateKey);
 
   const publicKey = await ed.getPublicKeyAsync(privateKey);
